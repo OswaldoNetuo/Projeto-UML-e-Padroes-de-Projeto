be760b1878f405f70a9d4815d66a4bfaead3feb4
Isa <osowskiisabela@gmail.com>
2024-06-02 13:52:38 -0300

    1st commit

---
+++ src/main/java/Produto/ProdutoFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Produto;
+
+import Categoria.Categoria;
+
+public class ProdutoFactory {
+    public void criaProduto(int produto_id, String nome, String descricao, double preco, Categoria categoria) {
+        
+    }
+}
